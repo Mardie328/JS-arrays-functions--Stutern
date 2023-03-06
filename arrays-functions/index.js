acbcac8bc66cd095601f50a38000ad20694d50e8
Mardie328 <tolayinka328@gmail.com>
2023-03-06 11:16:34 +0100

    add new file

---
+++ arrays-functions/index.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// Question 1
+// Mutating array methods are methods that modify the original array.
+// They are methods that change the array in place and return the modified array while Non-mutating array methods, on the other hand, do not modify the original array.
+// They are methods that return a new array based on the original array, leaving the original array unchanged.
+// Examples of mutating array methods are: push(), pop(), shift(), unshift(), splice()
+// Examples of non-mutating array methods are: slice(), concat(), filter(), map(), reduce()
+
+// Question 2
+const languages = ["C#", "JavaScript", "Ruby", "PHP", "Python"];
+languages.push("Kotlin");
+console.log(languages);
